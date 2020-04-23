667498f7942ccee0a1f53b98a99ee6c92de9d274
Christian Teeples <36543358+cteeples@users.noreply.github.com>
2020-04-23 17:38:33 -0500

    Updates

---
+++ precache-manifest.a253c5203b20729f9fc4aa6a94284e25.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "238aa6e545d86fe628bb1b3ab9a6b8c2",
+    "url": "/demoTMA/index.html"
+  },
+  {
+    "revision": "6b6d3b9c1cb5e1d4c9e8",
+    "url": "/demoTMA/static/css/2.cbf16948.chunk.css"
+  },
+  {
+    "revision": "900957869a1e0218b43d",
+    "url": "/demoTMA/static/css/main.3d95f57a.chunk.css"
+  },
+  {
+    "revision": "6b6d3b9c1cb5e1d4c9e8",
+    "url": "/demoTMA/static/js/2.9761c6d8.chunk.js"
+  },
+  {
+    "revision": "be169be4a3e874518ad6832abe30a674",
+    "url": "/demoTMA/static/js/2.9761c6d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "900957869a1e0218b43d",
+    "url": "/demoTMA/static/js/main.6db298a0.chunk.js"
+  },
+  {
+    "revision": "dec1246e913ff46f4527",
+    "url": "/demoTMA/static/js/runtime-main.300adbdc.js"
+  },
+  {
+    "revision": "b96593194a6e43855036b5817415fc9c",
+    "url": "/demoTMA/static/media/BalletClass.b9659319.jpg"
+  },
+  {
+    "revision": "a985648b2075593cc47ae99c5c5dfaea",
+    "url": "/demoTMA/static/media/Batala_band_02_Videvo.a985648b.mp4"
+  },
+  {
+    "revision": "0a7282dc58297135fe1638ed683f0203",
+    "url": "/demoTMA/static/media/angela.0a7282dc.jpg"
+  },
+  {
+    "revision": "68483556fef6de57a6ce9bd7dc3ba2f3",
+    "url": "/demoTMA/static/media/ballet_dancer.68483556.png"
+  },
+  {
+    "revision": "8d80c7e52c4c2356c8b450b197f0b377",
+    "url": "/demoTMA/static/media/band.8d80c7e5.png"
+  },
+  {
+    "revision": "cddeb98b3d1846395a3c0f859e61e591",
+    "url": "/demoTMA/static/media/cassie.cddeb98b.jpg"
+  },
+  {
+    "revision": "5b6523828f462ead7469c3582839c9c3",
+    "url": "/demoTMA/static/media/demo.5b652382.jpg"
+  },
+  {
+    "revision": "33c50a4d423a09fe715d3d18bd55c085",
+    "url": "/demoTMA/static/media/demo2.33c50a4d.jpg"
+  },
+  {
+    "revision": "90875ea34dbcd11482d88c0e4a0c1a02",
+    "url": "/demoTMA/static/media/jai.90875ea3.jpg"
+  },
+  {
+    "revision": "fd16f19f2f998034cc3ac974bec29bfc",
+    "url": "/demoTMA/static/media/jamie.fd16f19f.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demoTMA/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0c21a849a5a335ca19aa5ca1637b945a",
+    "url": "/demoTMA/static/media/logo3.0c21a849.jpg"
+  },
+  {
+    "revision": "8da84926871bb114f1668ddaaa0ad4ba",
+    "url": "/demoTMA/static/media/register-girl.8da84926.jpg"
+  },
+  {
+    "revision": "377ad11f1ecb8b9cfab6bee00ac5a0d3",
+    "url": "/demoTMA/static/media/team.377ad11f.jpg"
+  }
+]);
